50371a29c2e317a82e64dc713e4ce1dd720667ae
Teepluss <admin@jquerytips.com>
2013-11-01 01:09:58 +0700

    change donation btn

---
+++ tests/CategorizeTest.php
@@ -0,0 +1,19 @@
+<?php namespace Teepluss\Categorize\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class CategorizeTest extends PHPUnit_Framework_TestCase {
+
+    protected $categorize;
+
+    public function setUp()
+    {
+
+    }
+
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+}
