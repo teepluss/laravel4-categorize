6d04cdd65fdda60d02edc436c316e22a0ec4b7e7
Patrick Bielen <patrick@byorder.nl>
2014-01-08 15:00:39 +0100

    Update 2013_09_05_041254_create_categories_table.php
    Added image, position and active

--- src/migrations/2013_09_05_041254_create_categories_table.php
+++ src/migrations/2013_09_05_041254_create_categories_table.php
@@ -4,34 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCategoriesTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function($table)
 	{
-		Schema::create('categories', function($table)
-		{
-			$table->engine = 'InnoDB';
+	    $table->engine = 'InnoDB';
 
-		    $table->increments('id');
-		    $table->string('type', 100)->index();
-		    $table->string('title');
-		    $table->text('description');
-		    $table->integer('weight')->index();
-		    $table->timestamps();
-		});
-	}
+	    $table->increments('id');
+	    $table->string('type', 100)->index();
+	    $table->string('title');
+	    $table->text('description');
+	    $table->string('image', 100);
+            $table->integer('position');
+            $table->boolean('active');
+	    $table->integer('weight')->index();
+	    $table->timestamps();
+	});
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('categories');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	Schema::drop('categories');
+    }
 
 }
